ad22131f024b0781debd21c51c8b084454490607
dannytom <thedannytom@gmail.com>
2020-08-02 11:58:20 -0700

    fix broken string user type error

--- charon.py
+++ charon.py
@@ -21,30 +21,36 @@ async def on_message(message):
     for role in roles.ROLES_LIST:
         if message.content == '!iam ' + role.lower():
             try:
-                if(role not in message.author.roles):
-                    await message.author.add_roles(role)
-                    await message.channel.send(f'{message.author.name}, you are'
-                                               f' granted {role}')
+                newRole = discord.utils.get(message.guild.roles, name=role)
+                if(newRole not in message.author.roles):
+                    await message.author.add_roles(newRole)
+                    await message.channel.send(f'{message.author.name}, you'
+                                               f' are granted {role}')
                 else:
-                    await message.channel.send(f'{message.author.name}, but you'
-                                               f' are already {role}')
+                    await message.channel.send(f'{message.author.name}, but '
+                                               f' you are already {role}')
             except AttributeError:
-                await message.channel.send(f'{message.author.name}, that role does'
-                                           ' not exist or I have not been given '
-                                           ' permission to grant you that role')
+                await message.channel.send(f'{message.author.name}, that role'
+                                           ' does not exist or I have not been'
+                                           ' given permission to grant you'
+                                           ' that role')
 
-        if message.content == '!iamnot ' + role.lower():
-            try:
-                if(role in message.author.roles):
-                    await message.author.remove_roles(role)
-                    await message.channel.send(f'{message.author.name}, you are'
-                                               f' removed from {role}')
-                else:
-                    await message.channel.send(f'{message.author.name}, but you'
-                                               f' were never {role}')
-            except AttributeError:
-                await message.channel.send(f'{message.author.name}, that role does'
-                                           ' not exist or I have not been given'
-                                           ' permission to grant you that role')
+            if message.content == '!iamnot ' + role.lower():
+                newRole = discord.utils.get(message.guild.roles, name=role)
+                try:
+                    if(newRole in message.author.roles):
+                        await message.author.remove_roles(newRole)
+                        await message.channel.send(f'{message.author.name},'
+                                                   f' you are removed from'
+                                                   f' {role}')
+                    else:
+                        await message.channel.send(f'{message.author.name},'
+                                                   f' but you were never'
+                                                   f' {role}')
+                except AttributeError:
+                    await message.channel.send(f'{message.author.name}, that'
+                                               ' role does not exist or I have'
+                                               ' not been given permission to'
+                                               ' grant you that role')
 
 bot.run(TOKEN)
