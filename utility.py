33d84a1eaeed03ef93555356e5bc952b41dad4ab
dannytom <thedannytom@gmail.com>
2020-08-04 00:51:37 -0700

    ValueError

--- utility.py
+++ utility.py
@@ -4,10 +4,11 @@ import roles
 
 lowerList = [e.casefold() for e in roles.ROLES_LIST]
 
-def getRoleFromString(roleString):
+
+def getRoleFromString(ctx, roleString):
     if roleString.casefold() in lowerList:
-        index = lowerList.index(roleInput.casefold())
-        newRole = discord.utils.get(message.guild.roles,
+        index = lowerList.index(roleString.casefold())
+        newRole = discord.utils.get(ctx.guild.roles,
                                     name=roles.ROLES_LIST[index])
     else:
-        return None
+        raise ValueError('Invalid role string passed in by user')
