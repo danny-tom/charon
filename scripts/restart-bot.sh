df34e30bab609e013c83b8fd092346d336e3ff57
dannytom <thedannytom@gmail.com>
2020-08-06 20:58:08 -0700

    echo everything

--- scripts/restart-bot.sh
+++ scripts/restart-bot.sh
@@ -3,27 +3,27 @@
 # Kill the running processes of tmux if present
 if pgrep tmux
 then
-    sudo pkill tmux
+    echo sudo pkill tmux
 fi
 
 # Install the dependencies
 if pip3 list | grep python3-dotenv
 then
-    pip3 install python-dotenv 
+    echo pip3 install python-dotenv 
 fi
 
 if  pip3 list | grep discord.py
 then
-    pip3 install discord.py
+    echo pip3 install discord.py
 fi
 
 # Clone the repository
-git clone https://github.com/danny-tom/charon.git
-cd charon
+echo $(git clone https://github.com/danny-tom/charon.git)
+echo cd charon
 
 # Replace the TOKEN with our secret
-sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env
+echo sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env
 
 # Start new tmux and run the process
-tmux new -d
-tmux send-keys -t 0 "python3 charon.py" ENTER
+echo tmux new -d
+echo tmux send-keys -t 0 "python3 charon.py" ENTER
