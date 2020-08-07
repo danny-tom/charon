0f86f852bd3cdf2108af0d29b08840fe0e9a74ff
dannytom <thedannytom@gmail.com>
2020-08-06 21:05:37 -0700

    maybe put them in parens

--- scripts/restart-bot.sh
+++ scripts/restart-bot.sh
@@ -9,21 +9,21 @@ fi
 # Install the dependencies
 if pip3 list | grep python3-dotenv
 then
-    echo pip3 install python-dotenv 
+    echo $(pip3 install python-dotenv)
 fi
 
 if  pip3 list | grep discord.py
 then
-    echo pip3 install discord.py
+    echo $(pip3 install discord.py)
 fi
 
 # Clone the repository
 echo $(git clone https://github.com/danny-tom/charon.git)
-echo cd charon
+echo $(cd charon)
 
 # Replace the TOKEN with our secret
-echo sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env
+echo $(sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env)
 
 # Start new tmux and run the process
-echo tmux new -d
-echo tmux send-keys -t 0 "python3 charon.py" ENTER
+echo $(tmux new -d)
+echo $(tmux send-keys -t 0 "python3 charon.py" ENTER)
