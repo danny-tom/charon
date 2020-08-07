4584f923448cce135b0fee21f4d780cb2d7d1c18
dannytom <thedannytom@gmail.com>
2020-08-06 21:26:31 -0700

    run the echo command in the workflow and not the script

--- scripts/restart-bot.sh
+++ scripts/restart-bot.sh
@@ -3,27 +3,27 @@
 # Kill the running processes of tmux if present
 if pgrep tmux
 then
-    echo sudo pkill tmux
+    sudo pkill tmux
 fi
 
 # Install the dependencies
 if pip3 list | grep python3-dotenv
 then
-    echo $(pip3 install python-dotenv)
+    pip3 install python-dotenv
 fi
 
 if  pip3 list | grep discord.py
 then
-    echo $(pip3 install discord.py)
+    pip3 install discord.py
 fi
 
 # Clone the repository
-echo $(git clone https://github.com/danny-tom/charon.git)
-echo $(cd charon)
+git clone https://github.com/danny-tom/charon.git
+cd charon
 
 # Replace the TOKEN with our secret
-echo $(sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env)
+sed -i 's/###TOKEN HERE###/${TEST_TOKEN}/g' .env
 
 # Start new tmux and run the process
-echo $(tmux new -d)
-echo $(tmux send-keys -t 0 "python3 charon.py" ENTER)
+tmux new -d
+tmux send-keys -t 0 "python3 charon.py" ENTER
