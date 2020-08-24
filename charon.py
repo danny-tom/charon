82a4e6a8a553c537faace76fdd3f3603c6afb323
MyChakoTaco <charles.k.hsueh@gmail.com>
2020-08-24 00:22:12 -0700

    Refactor code to use bot.command
    - Major refactor to format code to match that of discord.py's bot.command structure
    - Remove try/catch for common branches and instead anticipate incorrect argument input
    - Remove regex as it's no longer needed
    - Format 'games' command to have string padding so that number of users per game line up nicely

--- charon.py
+++ charon.py
@@ -1,211 +1,143 @@
 # charon.py
 import os
-import discord.ext.commands.bot
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import roles
-import re
 import random
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 COMMAND_PREFIX = '.'
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
-bot = discord.ext.commands.Bot(COMMAND_PREFIX)
+# returns the role regardless of input case-sensitivity
+# returns None if no match can be found
+def getRoleID(roles, roleName):
+    for r in roles:
+        if roleName.lower() == str(r).lower():
+            return r
+    return None
 
-lowerList = [e.casefold() for e in roles.ROLES_LIST]
+# restricts roles that can be picked based on roles.py
+def isRestrictedRole(role):
+    for validRole in roles.ROLES_LIST:
+        if validRole.lower() == str(role).lower():
+            return False
+    return True
 
 # On any message, if the exact text is '.iam valorant' or
 # '.iamnot valorant', the bot will grant the role matching the String
 # stored in roles.py. The bot will message on errors such as
 # role name not found in server or the bot does not have enough permissions
-
-def iam_helper(message):
-    roleInput = message.content.split(" ", 1)[1]
-    # Ignore case sensitivity for the role
-    if roleInput.casefold() in lowerList:
-        roleIndex = lowerList.index(roleInput.casefold())
-        newRole = discord.utils.get(message.guild.roles,
-                                    name=roles.ROLES_LIST[roleIndex])
-        return True, newRole, roleIndex
-    return False, "", -1
-
-async def iam(message):
-    try:
-        role_found, newRole, index = iam_helper(message)
-
-        if (role_found):
-            try:
-                if(newRole not in message.author.roles):
-                    await message.author.add_roles(newRole)
-                    await message.channel.send(
-                        f'{message.author.name}, you are granted'
-                        f' {roles.ROLES_LIST[index]}')
-                else:
-                    await message.channel.send(
-                        f'{message.author.name}, but you are already'
-                        f' {roles.ROLES_LIST[index]}')
-            except AttributeError:
-                await message.channel.send(
-                    f'{message.author.name}, that role does not exist or'
-                    ' I have not been given permission to grant you that'
-                    ' role')
-        else:
-            await message.channel.send(
-                f'{message.author.name}, that role does not exist')
-    except IndexError:
-        await message.channel.send(
-            f'{message.author.name}, please tell me what role to use for iam')
-
-
-async def iamnot(message):
-    try:
-        role_found, newRole, index = iam_helper(message)
-            
-        if (role_found):
-            try:
-                if(newRole in message.author.roles):
-                    await message.author.remove_roles(newRole)
-                    await message.channel.send(
-                        f'{message.author.name}, you are removed from'
-                        f' {roles.ROLES_LIST[index]}')
-                else:
-                    await message.channel.send(
-                        f'{message.author.name}, but you were never'
-                        f' {roles.ROLES_LIST[index]}')
-            except AttributeError:
-                await message.channel.send(
-                    f'{message.author.name}, that role does not exist or I'
-                    'have not been given permission to grant you that'
-                    ' role')
-        else:
-            await message.channel.send(
-                f'{message.author.name}, that role does not exist')
-    except IndexError:
-        await message.channel.send(
-            f'{message.author.name}, please tell me what role to use for iamnot')
-
-async def whois(message):
-    # After the command, the 2nd part is the role name
-    try:
-        roleInput = message.content.split(" ", 1)[1]
-
-        # Ignore case sensitivity for the role
-        # Check if the bot has should be able to give that particular role
-        # information
-        try:
-            index = lowerList.index(roleInput.casefold())
-            searchedRole = discord.utils.get(message.guild.roles,
-                                                name=roles.ROLES_LIST[index])
-        except ValueError:
-            await message.channel.send(
-                f'{message.author.name}, that role does not exist or I have'
-                ' not been given permission to give you that information')
-
-        if roleInput.casefold() in lowerList:
-            try:
-                membersList = searchedRole.members
-                membersWithRole = list(member.name for member in membersList)
-                sortedList = sorted(membersWithRole, key=str.casefold)
-                membersWithRoleStr = "\n"
-                membersWithRoleStr = membersWithRoleStr.join(sortedList)
-                await message.channel.send(
-                    f'{message.author.name}, here is the list of users in'
-                    f' {roles.ROLES_LIST[index]} you requested:'
-                    f'```{membersWithRoleStr}```')
-            except AttributeError:
-                await message.channel.send(
-                    f'{message.author.name}, that role does not exist or I'
-                    ' have not been given permission to give you that'
-                    ' information')
-    except IndexError:
-        await message.channel.send(
-            f'{message.author.name}, please tell me what role to use for whois')
-
-async def games(message):
-    sortedList = sorted(roles.ROLES_LIST, key=str.casefold)
-    registeredUsersList = []
-    newGamesList = []
-
-    try:
-        for role in sortedList:
-            if discord.utils.get(message.guild.roles, name=role) is None:
-                pass
-            else:
-                registeredUsers = len(discord.utils.get(
-                    message.guild.roles, name=role).members)
-                newGamesList.append(role)
-                registeredUsersList.append(registeredUsers)
-
-        gameAndCount = list(zip(newGamesList, registeredUsersList))
-        gameAndCountStr = "\n"
-
-        for pair in gameAndCount:
-            gameAndCountStr += pair[0] + '\t(' + str(pair[1]) + ')\n'
-
-        outputString = f'{message.author.name}, here is a list of the' \
-            f' roles that I manage: ```{gameAndCountStr}```'
-
-        if len(gameAndCount) == 0:
-            await message.channel.send(f'{message.author.name}, I manage'
-                                        ' no games here')
-        else:
-            await message.channel.send(outputString)
-
-    except AttributeError:
-        await message.channel.send(
-            f'{message.author.name}, there was an issue finding the games'
-            'that this server supports')
-
-
-async def charon_help(message):
-    command_list = '\n'+'\n'.join(LIST_OF_COMMANDS.keys())
-
-    if (message.content.count(" ") == 1):
-        command = message.content.split(" ")[1]
-        if (command in LIST_OF_COMMANDS.keys()):
-            await message.channel.send(f'{message.author.name}, here is what I know' 
-                f' about {command}: ```{LIST_OF_COMMANDS[command].__doc__}```')
-        else:
-            await message.channel.send(f'{message.author.name}, I have not heard of ' 
-                'that command. Here is a list of the commands that I know:'
-                f'```{command_list}```')
-    else:
-        await message.channel.send(f'{message.author.name}, Here is a list of the '
-            f'commands that I know: ```{command_list}```')
-
-
-LIST_OF_COMMANDS = {
-    "iam":iam,
-    "iamnot":iamnot,
-    "whois":whois,
-    "games":games,
-    "help":charon_help
-                    }
-
-iam.__doc__ = f'''Usage: \"{COMMAND_PREFIX}iam SomeRole\" - Add yourself to SomeRole'''
-iamnot.__doc__ = f'''Usage: \"{COMMAND_PREFIX}iamnot SomeRole\" - Remove yourself from SomeRole'''
-whois.__doc__ = f'''Usage: \"{COMMAND_PREFIX}whois SomeRole\" - Prints a list of users belonging to SomeRole'''
-games.__doc__ = f'''Usage: \"{COMMAND_PREFIX}games\" - Prints a list of my supported roles'''
-charon_help.__doc__ = f'''Usage: \"{COMMAND_PREFIX}help\" - Prints a list of supported commands\n\
-    \"{COMMAND_PREFIX}help SomeCommand\" - Prints help for SomeCommand'''
-
-@bot.event
-async def on_message(message):
-    # Messages that should bring up the "unrecognized command" reply: ".foo", "."
-    # Messages that should not: "..", "..."
-    result = re.match(f'^\{COMMAND_PREFIX}([a-zA-Z0-9]+)', message.content)
-    if not result:
+@bot.command(name='iam', brief='Add yourself to SomeRole', 
+description=f'''\"{COMMAND_PREFIX}iam SomeRole\" - Add yourself to SomeRole''')
+async def iam(context, *arg):
+    if len(arg) == 0:
+        await context.channel.send(
+            f'{context.author.name}, please tell me what role to use for iam')
         return
 
-    command = result.group(1)
-    if command in LIST_OF_COMMANDS.keys():
-        # See above for command functions
-        await LIST_OF_COMMANDS[command](message)
+    role = getRoleID(context.guild.roles, arg[0])
+
+    if role == None:
+        await context.channel.send(
+            f'{context.author.name}, role \'{arg[0]}\' does not exist')
+        return
+
+    if isRestrictedRole(role):
+        await context.channel.send(
+            f'{context.author.name}, {role} is a restricted role')
+        return
+
+    if role in context.author.roles:
+        await context.channel.send(
+            f'{context.author.name}, but you are already {role}')
     else:
-        await message.channel.send(
-            f'{message.author.name}, that is an unrecognized command.\n'
-            f'For a list of supported commands, please send \"{COMMAND_PREFIX}help\"')
+        await context.author.add_roles(role)
+        await context.channel.send(
+            f'{context.author.name}, you are granted {role}')
+        
+@bot.command(name='iamnot', brief='Remove yourself from SomeRole',
+description=f'''\"{COMMAND_PREFIX}iamnot SomeRole\" - Remove yourself from SomeRole''')
+async def iamnot(context, *arg):
+    if len(arg) == 0:
+        await context.channel.send(
+            f'{context.author.name}, please tell me what role to use for iamnot')
+        return
+
+    role = getRoleID(context.guild.roles, arg[0])
+
+    if role == None:
+        await context.channel.send(
+            f'{context.author.name}, role \'{arg[0]}\' does not exist')
+        return
+
+    if role in context.author.roles:
+        await context.author.remove_roles(role)
+        await context.channel.send(
+            f'{context.author.name}, you are removed from {role}')
+    else:
+        await context.channel.send(
+            f'{context.author.name}, but you were never {role}')
+
+@bot.command(name='whois', brief='Prints users belonging to role',
+description=f'''\"{COMMAND_PREFIX}whois SomeRole\" - Prints a list of users belonging to SomeRole''')
+async def whois(context, *arg):
+    if len(arg) == 0:
+        await context.channel.send(
+            f'{context.author.name}, please tell me what role to use for whois')
+        return
+
+    role = getRoleID(context.guild.roles, arg[0])
+
+    if role == None:
+        await context.channel.send(
+            f'{context.author.name}, role \'{arg[0]}\' does not exist')
+        return
+
+    members = sorted(list(member.name for member in role.members), key=str.casefold)
+
+    if len(members) == 0:
+        await context.channel.send(
+            f'{context.author.name}, {role} has no users')
+        return
+
+    memberStr = "\n".join(members)
+
+    await context.channel.send(
+        f'{context.author.name}, here is the list of users in '
+        f'{role} you requested:'
+        f'```{memberStr}```')
+
+# Lists out games supported 
+@bot.command(name='games', brief='Prints my supported roles',
+description=f'''\"{COMMAND_PREFIX}games\" - Prints a list of my supported roles''')
+async def games(context):
+    games = []
+
+    # Variables are used for string formatting purposes
+    longestRole = 0
+    padding = 1
+
+    # Add valid roles to our display
+    for role in context.guild.roles:
+        if not isRestrictedRole(role):
+            games.append([str(role), str(len(role.members))])
+            if len(str(role)) > longestRole:
+                longestRole = len(str(role))
+
+    if len(games) == 0:
+        await context.channel.send(f'{context.author.name}, I manage no games here')
+        return
+    
+    formatter = "{:<"+str(longestRole+padding)+"}({})\n"
+    gamesToStr = ""
+    for pair in games:
+        gamesToStr += formatter.format(pair[0], pair[1])
+    await context.channel.send(f'{context.author.name}, here is a list of the' \
+        f' roles that I manage: ```{gamesToStr}```')
 
 @bot.event
 async def on_member_join(member):
