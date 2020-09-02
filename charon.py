f4cee6c5334b2aa63023a96c2aa3e30f98620921
dannytom <thedannytom@gmail.com>
2020-09-02 01:19:07 -0700

    add options for help, changing help to dm and no category to other

--- charon.py
+++ charon.py
@@ -15,8 +15,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 COMMAND_PREFIX = os.getenv('COMMAND_PREFIX')
 
+options = commands.DefaultHelpCommand(dm_help=True, no_category='Other')
+bot = commands.Bot(command_prefix=COMMAND_PREFIX,
+                   help_command=options)
 
-bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 logging.basicConfig(level=logging.INFO)
 
 
